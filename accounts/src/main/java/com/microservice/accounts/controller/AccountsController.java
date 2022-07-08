da16d4d030a84f549cbf9bfc9b68cb405afd5075
hsdanield <hsdanield@gmail.com>
2022-07-08 14:52:48 -0300

    Include Spring Cloud with Config Starter in Accounts

--- accounts/src/main/java/com/microservice/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservice/accounts/controller/AccountsController.java
@@ -1,9 +1,15 @@
 package com.microservice.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.microservice.accounts.config.AccountsServiceConfig;
 import com.microservice.accounts.model.Accounts;
 import com.microservice.accounts.model.Customer;
+import com.microservice.accounts.model.Properties;
 import com.microservice.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +20,24 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    private AccountsServiceConfig accountsServiceConfig;
+
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
         return accountsRepository.findByCustomerId(customer.getCustomerId());
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = Properties.builder()
+                .msg(accountsServiceConfig.getMsg())
+                .buildVersion(accountsServiceConfig.getBuildVersion())
+                .mailDetails(accountsServiceConfig.getMailDetails())
+                .activeBranches(accountsServiceConfig.getActivateBranches())
+                .build();
+        return ow.writeValueAsString(properties);
     }
 
 }
