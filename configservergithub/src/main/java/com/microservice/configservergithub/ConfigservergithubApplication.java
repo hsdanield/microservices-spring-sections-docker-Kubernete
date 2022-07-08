fe621550790c201a93ddb7e1baaaa20821bab284
hsdanield <hsdanield@gmail.com>
2022-07-08 13:36:39 -0300

    Create Config Server with Github

---
+++ configservergithub/src/main/java/com/microservice/configservergithub/ConfigservergithubApplication.java
@@ -0,0 +1,15 @@
+package com.microservice.configservergithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigservergithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigservergithubApplication.class, args);
+	}
+
+}
---
+++ configservergithub/src/test/java/com/microservice/configservergithub/ConfigservergithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.configservergithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigservergithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
