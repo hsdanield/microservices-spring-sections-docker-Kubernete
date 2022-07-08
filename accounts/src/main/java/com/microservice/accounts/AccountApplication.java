17fe39cfabf38a7c1e5583dcb59627a58d75ce30
hsdanield <hsdanield@gmail.com>
2022-07-08 18:18:54 -0300

    Include docker-compose, RefreshScop and Enctype/Descrypt example

--- accounts/src/main/java/com/microservice/accounts/AccountApplication.java
+++ accounts/src/main/java/com/microservice/accounts/AccountApplication.java
@@ -3,11 +3,13 @@ package com.microservice.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.microservice.accounts.controller") })
 @EnableJpaRepositories("com.microservice.accounts.repository")
 @EntityScan("com.microservice.accounts.model")
--- cards/src/main/java/com/microservice/cards/CardsApplication.java
+++ cards/src/main/java/com/microservice/cards/CardsApplication.java
@@ -3,11 +3,13 @@ package com.microservice.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.microservice.cards.controller") })
 @EnableJpaRepositories("com.microservice.cards.repository")
 @EntityScan("com.microservice.cards.model")
