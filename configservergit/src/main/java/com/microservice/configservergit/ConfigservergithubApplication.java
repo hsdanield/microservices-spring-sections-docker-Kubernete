214e1b609860f1aee954d72abe8e5bfbbb8889a1
hsdanield <hsdanield@gmail.com>
2022-07-10 17:00:00 -0300

    Ajust name configservergit

--- configservergit/src/main/java/com/microservice/configservergit/ConfigservergithubApplication.java
+++ configservergit/src/main/java/com/microservice/configservergit/ConfigservergithubApplication.java
@@ -1,4 +1,4 @@
-package com.microservice.configservergithub;
+package com.microservice.configservergit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- configservergit/src/test/java/com/microservice/configservergit/ConfigservergithubApplicationTests.java
+++ configservergit/src/test/java/com/microservice/configservergit/ConfigservergithubApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microservice.configservergithub;
+package com.microservice.configservergit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
